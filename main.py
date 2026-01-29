9b542973291e3126e4aa0536cb834ca7cd93c7ed
Muhhekaneok <wizardrad@gmail.com>
2026-01-29 23:26:44 +0700

    add 'delete_tasks' method

--- main.py
+++ main.py
@@ -23,13 +23,30 @@ class SmartTasker:
         if not self.tasks:
             print("There are no tasks to print")
             return
+
         print("---My tasks---")
-        print(f"I have {len(self.tasks)} tasks:" if len(self.tasks) > 1 else "I have 1 task:")
-        print(f"Tasks: {self.tasks}")
+        print(f"I have {len(self.tasks)} tasks sorted by priority:" if len(self.tasks) > 1
+              else "I have 1 task:")
+
+        sorted_tasks = sorted(self.tasks.items(), key=lambda x: x[1]["priority"])
+        for tid, data in sorted_tasks:
+            print(f"ID {tid} | {data["title"]} {data["priority"]} | tags: {data["tags"]}")
+        print("-" * 25)
+
+    def delete_task(self, task_id):
+        removed_task_id = self.tasks.pop(task_id, None)
+        if removed_task_id:
+            print(f"Task '{removed_task_id["title"]}' was removed")
+        else:
+            print(f"Task with ID {task_id} was not found")
 
 
 task_manager = SmartTasker()
 task_manager.add_task("Coding", 1, ["algorithms", "frameworks"])
 task_manager.add_task("German", 2, ["Grammatik", "Wörterbuch"])
 task_manager.add_task("English", 1, ["grammar", "vocabulary"])
+
+task_manager.print_tasks()
+
+task_manager.delete_task(3)
 task_manager.print_tasks()
