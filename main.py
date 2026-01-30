a4082017a7b76299d37202dd9835434edc0017ac
Muhhekaneok <wizardrad@gmail.com>
2026-01-31 00:52:49 +0700

    add 'save_to_file', 'load_from_file'

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
+import os
 import time
+from json import JSONDecodeError
 
 
 class SmartTasker:
@@ -6,6 +9,7 @@ class SmartTasker:
         self.tasks = {}
         self.undo_stack = []
         self.counter = 1
+        self.filepath = "tasks.json"
 
     def add_task(self, title, priority, tags):
         task_id = self.counter
@@ -30,23 +34,55 @@ class SmartTasker:
 
         sorted_tasks = sorted(self.tasks.items(), key=lambda x: x[1]["priority"])
         for tid, data in sorted_tasks:
-            print(f"ID {tid} | {data["title"]} {data["priority"]} | tags: {data["tags"]}")
+            print(f"ID {tid} | {data['title']} {data['priority']} | tags: {data['tags']}")
         print("-" * 25)
 
     def delete_task(self, task_id):
-        removed_task_id = self.tasks.pop(task_id, None)
-        if removed_task_id:
-            print(f"Task '{removed_task_id["title"]}' was removed")
+        removed_task = self.tasks.pop(task_id, None)
+        if removed_task:
+            print(f"Task '{removed_task['title']}' was removed")
         else:
             print(f"Task with ID {task_id} was not found")
 
+    def save_to_file(self):
+        clean_tasks = {}
+        for tid, data in self.tasks.items():
+            clean_tasks[tid] = data.copy()
+            clean_tasks[tid]["tags"] = list(data["tags"])
+
+        with open(self.filepath, "w", encoding="utf-8") as f:
+            json.dump(clean_tasks, f, indent=4)
+        print("The data has been saved to tasks.json")
+
+    def load_from_file(self):
+        if not os.path.exists(self.filepath):
+            print("The file doesn't exist")
+            return
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as f:
+                content = f.read()
+                if not content:
+                    print("File is empty")
+                    return
+                data = json.loads(content)
+            self.tasks = {int(k): v for k, v in data.items()}
+            self.counter = max(self.tasks.keys()) + 1
+
+            print(f"File has been loaded. Loaded {len(self.tasks)} tasks")
+        except JSONDecodeError:
+            print("File is damaged.")
+
 
 task_manager = SmartTasker()
-task_manager.add_task("Coding", 1, ["algorithms", "frameworks"])
-task_manager.add_task("German", 2, ["Grammatik", "Wörterbuch"])
-task_manager.add_task("English", 1, ["grammar", "vocabulary"])
+# task_manager.add_task("Coding", 1, ["algorithms", "frameworks"])
+# task_manager.add_task("German", 2, ["Grammatik", "Wörterbuch"])
+# task_manager.add_task("English", 1, ["grammar", "vocabulary"])
 
-task_manager.print_tasks()
+# task_manager.print_tasks()
 
-task_manager.delete_task(3)
-task_manager.print_tasks()
+# task_manager.delete_task(3)
+# task_manager.print_tasks()
+
+# task_manager.save_to_file()
+
+task_manager.load_from_file()
